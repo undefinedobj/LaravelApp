54d3cfad93c3e6e1de01a59a718e09fe81c0386a
undefinedobj <8wy701645@163.com>
2019-05-16 13:47:30 +0800

    repository

--- app/Repositories/DiscussionRepositoryEloquent.php
+++ app/Repositories/DiscussionRepositoryEloquent.php
@@ -5,7 +5,7 @@ namespace App\Repositories;
 use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Criteria\RequestCriteria;
 use App\Repositories\DiscussionRepository;
-use App\Models \Discussion;
+use App\Models\Discussion;
 use App\Validators\DiscussionValidator;
 
 /**
