a87c37a46066e5540a62e0fea54e643bacdeec6f
undefinedobj <8wy701645@163.com>
2019-05-26 17:06:45 +0800

    使用Vuejs实现实时评论

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta id="token" name="token" value=" {{ csrf_token() }} ">
+
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css">
     <link href="{{ asset('vendor/font-awesome-4.7.0/css/font-awesome.css') }}" rel="stylesheet">
--- resources/views/forum/show.blade.php
+++ resources/views/forum/show.blade.php
@@ -24,7 +24,7 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-9" role="main">
+            <div class="col-md-9" role="main" id="post">
                 <div class="blog-post">
 
                 </div><!-- /.blog-post -->
@@ -40,14 +40,27 @@
                                 <img class="media-object img-circle" src="{{ $comment->user->avatar }}" alt="64*64" style="width: 64px; height: 64px">
                             </a>
                         </div>
-                    </div>
 
-                    <div class="media-body">
+                        <div class="media-body">
                             <h4 class="media-heading">{{ $comment->user->name }}</h4>
                             {{ $comment->body }}
+                        </div>
                     </div>
                 @endforeach
+                    {{-- vue.js.star--}}
+                    <div class="media" v-for="comment in comments">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object img-circle" src="@{{ comment.avatar }}" alt="64*64" style="width: 64px; height: 64px">
+                            </a>
+                        </div>
 
+                        <div class="media-body">
+                            <h4 class="media-heading">@{{ comment.name }}</h4>
+                            @{{ comment.body }}
+                        </div>
+                    </div>
+                {{-- vue.js.end--}}
                 <hr>
 
                 {{-- 评论输框 --}}
@@ -60,14 +73,14 @@
                         </ul>
                     @endif
 
-                    {!! Form::open(['url' => '/comments', 'method' => 'post']) !!}
+                    {!! Form::open(['url' => '/comments', 'method' => 'post', 'v-on:submit' => 'onSubmitForm']) !!}
 
                         <div class="form-group">
                             {!! Form::hidden('discussion_id', $discussion->id, ['class' => 'form-control']) !!}
                         </div>
 
                         <div class="form-group">
-                            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+                            {!! Form::textarea('body', null, ['class' => 'form-control', 'v-model' => 'newComment.body']) !!}
                         </div>
 
                         <div>
@@ -81,4 +94,43 @@
             </div>
         </div>
     </div>
+
+    <script>
+        Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('value');
+
+        new Vue({
+            el: '#post',
+            data: {
+                comments: [],
+                newComment: {
+                    name: '{{ Auth::user()->name }}',
+                    avatar: '{{ Auth::user()->avatar }}',
+                    body: ''
+                },
+                newPost: {
+                    discussion_id: '{{ $discussion->id }}',
+                    user_id: '{{ Auth::user()->id }}',
+                    body: ''
+                }
+            },
+            methods: {
+                onsubmitForm:function (element) {
+                    element.preventDefault();
+                    var comment = this.newComment;
+                    var post = this.newPost;
+                    post.body = comment.body;
+
+                    this.$http.post('/comments', post, function () {
+                        this.comments.push(comment);
+                    });
+
+                    this.newComment = {
+                        name: '{{ Auth::user()->name }}',
+                        avatar: '{{ Auth::user()->avatar }}',
+                        body: ''
+                    };
+                }
+            }
+        });
+    </script>
 @endsection
