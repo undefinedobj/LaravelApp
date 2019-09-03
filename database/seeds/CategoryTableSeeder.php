a7fba07cd69f0405f55b88959e7680a48e92e219
undefinedobj <8wy701645@163.com>
2019-09-03 19:04:19 +0800

    CateGoryTableSedder

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -12,7 +12,7 @@ class CategoryTableSeeder extends Seeder
      */
     public function run()
     {
-        Category::truncate();
+//        Category::truncate();
 
         Category::insert([
             [
@@ -20,7 +20,7 @@ class CategoryTableSeeder extends Seeder
                 'order'     => 1,
                 'title'     => '后端',
                 'icon'      => 'fab fa-laravel',
-                'uri'       => '',
+                'uri'       => '/',
             ],
             [
                 'parent_id' => 0,
@@ -34,21 +34,21 @@ class CategoryTableSeeder extends Seeder
                 'order'     => 3,
                 'title'     => '服务器',
                 'icon'      => 'fas fa-server',
-                'uri'       => 'auth/users',
+                'uri'       => '/',
             ],
             [
                 'parent_id' => 0,
                 'order'     => 4,
                 'title'     => '工具',
                 'icon'      => 'fa fa-wrench',
-                'uri'       => 'auth/roles',
+                'uri'       => '/',
             ],
             [
                 'parent_id' => 0,
                 'order'     => 5,
                 'title'     => '其它',
                 'icon'      => 'fas fa-cube',
-                'uri'       => 'auth/permissions',
+                'uri'       => '/',
             ],
             [
                 'parent_id' => 0,
@@ -61,102 +61,119 @@ class CategoryTableSeeder extends Seeder
                 'parent_id' => 1,
                 'order'     => 7,
                 'title'     => 'PHP',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 1,
                 'order'     => 8,
                 'title'     => 'GoLang',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 1,
                 'order'     => 9,
                 'title'     => 'Laravel',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 1,
                 'order'     => 10,
                 'title'     => 'ThinkPHP',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 1,
                 'order'     => 11,
                 'title'     => 'Yii',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 2,
                 'order'     => 12,
                 'title'     => 'Node.js',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 2,
                 'order'     => 13,
                 'title'     => 'JavaScript',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 2,
                 'order'     => 14,
                 'title'     => 'Vue',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 3,
                 'order'     => 15,
                 'title'     => 'Nginx',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 3,
                 'order'     => 16,
                 'title'     => 'Apache',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 4,
                 'order'     => 17,
                 'title'     => 'MySQL',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 4,
                 'order'     => 18,
                 'title'     => 'SQLServer',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 4,
                 'order'     => 19,
                 'title'     => 'Redis',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 4,
                 'order'     => 20,
                 'title'     => 'Memcache',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 5,
                 'order'     => 21,
                 'title'     => 'PHPStorm',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 5,
                 'order'     => 22,
                 'title'     => 'Git',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
             [
                 'parent_id' => 6,
                 'order'     => 23,
                 'title'     => '生活笔记',
+                'icon'      => 'glyphicon glyphicon-align-justify',
                 'uri'       => '/',
             ],
         ]);
