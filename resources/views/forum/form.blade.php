9c9d94b78ac14285ca93e35b3091d79d70539455
undefinedobj <8wy701645@163.com>
2019-09-20 10:09:18 +0800

    图片上传预览

--- resources/views/forum/form.blade.php
+++ resources/views/forum/form.blade.php
@@ -23,8 +23,9 @@
 
 <div class="form-group">
     {!! Form::label('img', 'Image(445 * 190)') !!}
-    {!! Form::file('img', ['class' => 'form-control','id'=>'form-control']) !!}
+    {!! Form::file('img', ['class' => 'form-control','id'=>'image-upload']) !!}
 </div>
+{!! Form::image($discussion->img ?? '/images/default-avatar.jpg', null, ['id'=>'image-view', 'width'=>"160", 'height'=>"160"]) !!}
 
 <div class="form-group">
     {!! Form::label('order', 'Order') !!}
@@ -34,3 +35,17 @@
 <div class="form-group">
     {!! Form::hidden('hidden-img',$discussion->img ?? null, ['class' => 'form-control','id'=>'form-control']) !!}
 </div>
+
+<script type="text/javascript">
+    $(function() {
+        $("#image-upload").change(function() {
+            var $file = $(this);
+            var objUrl = $file[0].files[0];
+            //获得一个http格式的url路径:mozilla(firefox)||webkit or chrome
+            var windowURL = window.URL || window.webkitURL;
+            //createObjectURL创建一个指向该参数对象(图片)的URL
+            var dataURL = windowURL.createObjectURL(objUrl);
+            $("#image-view").attr("src", dataURL);
+        });
+    });
+</script>
