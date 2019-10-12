c7e81d22b35c19fe235a1c5c3535694b5d04a61a
Savory <8wy701645@163.com>
2019-10-12 11:47:51 +0800

    微博登录

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthenticationController extends Controller
 {
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,8 @@ class CreateUsersTable extends Migration
             $table->string('confirm_code',64)->comment('验证码');
             $table->integer('is_confirmed')->default(1)->comment('是否验证,0否,1是');
             $table->timestamp('email_verified_at')->nullable()->comment('验证时间');
+            $table->string('provider');
+            $table->integer('provider_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
